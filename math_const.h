632dec1bb8612682f6f60d37d940c422b9eabb10
Ed Smith-Rowland <3dw4rd@verizon.net>
2017-08-25 12:13:27 -0400

    Change _t to _v

--- math_const.h
+++ math_const.h
@@ -5,224 +5,224 @@ namespace math_constants {
 
   /// Euler constant @f$ e = 2.71828... @f$
   template<typename _Tp>
-    inline constexpr _Tp e_t;
+    inline constexpr _Tp e_v;
 
   /// The base-2 logarithm of the Euler constant @f$ log_2(e) @f$
   template<typename _Tp>
-    inline constexpr _Tp log2e_t;
+    inline constexpr _Tp log2e_v;
 
   /// The base-10 logarithm of the Euler constant @f$ log_{10}(e) @f$
   template<typename _Tp>
-    inline constexpr _Tp log10e_t;
+    inline constexpr _Tp log10e_v;
 
   /// The Archimedean constant @f$ \pi = 3.1415... @f$
   template<typename _Tp>
-    inline constexpr _Tp pi_t;
+    inline constexpr _Tp pi_v;
 
   /// The reciprocal of Archimedes' constant @f$ 1/\pi @f$
   template<typename _Tp>
-    inline constexpr _Tp invpi_t;
+    inline constexpr _Tp invpi_v;
 
   /// The reciprocal square root of Archimedes' constant @f$ 1/\sqrt{\pi} @f$
   template<typename _Tp>
-    inline constexpr _Tp invsqrtpi_t;
+    inline constexpr _Tp invsqrtpi_v;
 
   /// The Napierian logarithm of 2 @f$ ln(2) @f$
   template<typename _Tp>
-    inline constexpr _Tp ln2_t;
+    inline constexpr _Tp ln2_v;
 
   /// The Napierian logarithm of 10 @f$ ln(10) @f$
   template<typename _Tp>
-    inline constexpr _Tp ln10_t;
+    inline constexpr _Tp ln10_v;
 
   /// The square root of 2 @f$ \sqrt{2} @f$
   template<typename _Tp>
-    inline constexpr _Tp sqrt2_t;
+    inline constexpr _Tp sqrt2_v;
 
   /// The square root of 3 @f$ \sqrt{3} @f$
   template<typename _Tp>
-    inline constexpr _Tp sqrt3_t;
+    inline constexpr _Tp sqrt3_v;
 
   /// The reciprocal square root of 3 @f$ 1 / \sqrt{3} @f$
   template<typename _Tp>
-    inline constexpr _Tp invsqrt3_t;
+    inline constexpr _Tp invsqrt3_v;
 
   /// The radians per degree conversion factor @f$ \pi / 180 @f$
   template<typename _Tp>
-    inline constexpr _Tp radian_t;
+    inline constexpr _Tp radian_v;
 
   /// The Euler constant @f$ \gamma_E @f$
   template<typename _Tp>
-    inline constexpr _Tp egamma_t;
+    inline constexpr _Tp egamma_v;
 
   /// The golden ratio @f$ \phi = (\sqrt{5} + 1)/2 @f$
   template<typename _Tp>
-    inline constexpr _Tp phi_t;
+    inline constexpr _Tp phi_v;
 
   /// Catalan's constant @f$ @f$ G = 1 - 1/9 + 1/25 - 1/49 + 1/81 - ... @f$
   template<typename _Tp>
-    inline constexpr _Tp catalan_t;
+    inline constexpr _Tp catalan_v;
 
   /// Ape'ry's constant @f$ \zeta(3) = \pi^2/6 @f$
   template<typename _Tp>
-    inline constexpr _Tp apery_t;
+    inline constexpr _Tp apery_v;
 
   /// Glaisher's constant @f$  @f$
   template<typename _Tp>
-    inline constexpr _Tp glaisher_t;
+    inline constexpr _Tp glaisher_v;
 
   inline constexpr float
-  ef = e_t<float>;
+  ef = e_v<float>;
 
   inline constexpr float
-  log2ef = log2e_t<float>;
+  log2ef = log2e_v<float>;
 
   inline constexpr float
-  log10ef = log10e_t<float>;
+  log10ef = log10e_v<float>;
 
   inline constexpr float
-  pif = pi_t<float>;
+  pif = pi_v<float>;
 
   inline constexpr float
-  invpif = invpi_t<float>;
+  invpif = invpi_v<float>;
 
   inline constexpr float
-  invsqrtpif = invsqrtpi_t<float>;
+  invsqrtpif = invsqrtpi_v<float>;
 
   inline constexpr float
-  ln2f = ln2_t<float>;
+  ln2f = ln2_v<float>;
 
   inline constexpr float
-  ln10f = ln10_t<float>;
+  ln10f = ln10_v<float>;
 
   inline constexpr float
-  sqrt2f = sqrt2_t<float>;
+  sqrt2f = sqrt2_v<float>;
 
   inline constexpr float
-  sqrt3f = sqrt3_t<float>;
+  sqrt3f = sqrt3_v<float>;
 
   inline constexpr float
-  invsqrt3f = invsqrt3_t<float>;
+  invsqrt3f = invsqrt3_v<float>;
 
   inline constexpr float
-  radianf = radian_t<float>;
+  radianf = radian_v<float>;
 
   inline constexpr float
-  egammaf = egamma_t<float>;
+  egammaf = egamma_v<float>;
 
   inline constexpr float
-  phif = phi_t<float>;
+  phif = phi_v<float>;
 
   inline constexpr float
-  catalanf = catalan_t<float>;
+  catalanf = catalan_v<float>;
 
   inline constexpr float
-  aperyf = apery_t<float>;
+  aperyf = apery_v<float>;
 
   inline constexpr float
-  glaisherf = glaisher_t<float>;
+  glaisherf = glaisher_v<float>;
 
   inline constexpr double
-  e = e_t<double>;
+  e = e_v<double>;
 
   inline constexpr double
-  log2e = log2e_t<double>;
+  log2e = log2e_v<double>;
 
   inline constexpr double
-  log10e = log10e_t<double>;
+  log10e = log10e_v<double>;
 
   inline constexpr double
-  pi = pi_t<double>;
+  pi = pi_v<double>;
 
   inline constexpr double
-  invpi = invpi_t<double>;
+  invpi = invpi_v<double>;
 
   inline constexpr double
-  invsqrtpi = invsqrtpi_t<double>;
+  invsqrtpi = invsqrtpi_v<double>;
 
   inline constexpr double
-  ln2 = ln2_t<double>;
+  ln2 = ln2_v<double>;
 
   inline constexpr double
-  ln10 = ln10_t<double>;
+  ln10 = ln10_v<double>;
 
   inline constexpr double
-  sqrt2 = sqrt2_t<double>;
+  sqrt2 = sqrt2_v<double>;
 
   inline constexpr double
-  sqrt3 = sqrt3_t<double>;
+  sqrt3 = sqrt3_v<double>;
 
   inline constexpr double
-  invsqrt3 = invsqrt3_t<double>;
+  invsqrt3 = invsqrt3_v<double>;
 
   inline constexpr double
-  radian = radian_t<double>;
+  radian = radian_v<double>;
 
   inline constexpr double
-  egamma = egamma_t<double>;
+  egamma = egamma_v<double>;
 
   inline constexpr double
-  phi = phi_t<double>;
+  phi = phi_v<double>;
 
   inline constexpr double
-  catalan = catalan_t<double>;
+  catalan = catalan_v<double>;
 
   inline constexpr double
-  apery = apery_t<double>;
+  apery = apery_v<double>;
 
   inline constexpr double
-  glaisher = glaisher_t<double>;
+  glaisher = glaisher_v<double>;
 
   inline constexpr long double
-  el = e_t<long double>;
+  el = e_v<long double>;
 
   inline constexpr long double
-  log2el = log2e_t<long double>;
+  log2el = log2e_v<long double>;
 
   inline constexpr long double
-  log10el = log10e_t<long double>;
+  log10el = log10e_v<long double>;
 
   inline constexpr long double
-  pil = pi_t<long double>;
+  pil = pi_v<long double>;
 
   inline constexpr long double
-  invpil = invpi_t<long double>;
+  invpil = invpi_v<long double>;
 
   inline constexpr long double
-  invsqrtpil = invsqrtpi_t<long double>;
+  invsqrtpil = invsqrtpi_v<long double>;
 
   inline constexpr long double
-  ln2l = ln2_t<long double>;
+  ln2l = ln2_v<long double>;
 
   inline constexpr long double
-  ln10l = ln10_t<long double>;
+  ln10l = ln10_v<long double>;
 
   inline constexpr long double
-  sqrt2l = sqrt2_t<long double>;
+  sqrt2l = sqrt2_v<long double>;
 
   inline constexpr long double
-  sqrt3l = sqrt3_t<long double>;
+  sqrt3l = sqrt3_v<long double>;
 
   inline constexpr long double
-  invsqrt3l = invsqrt3_t<long double>;
+  invsqrt3l = invsqrt3_v<long double>;
 
   inline constexpr long double
-  radianl = radian_t<long double>;
+  radianl = radian_v<long double>;
 
   inline constexpr long double
-  egammal = egamma_t<long double>;
+  egammal = egamma_v<long double>;
 
   inline constexpr long double
-  phil = phi_t<long double>;
+  phil = phi_v<long double>;
 
   inline constexpr long double
-  catalanl = catalan_t<long double>;
+  catalanl = catalan_v<long double>;
 
   inline constexpr long double
-  aperyl = apery_t<long double>;
+  aperyl = apery_v<long double>;
 
   inline constexpr long double
-  glaisherl = glaisher_t<long double>;
+  glaisherl = glaisher_v<long double>;
 
 } // namespace math_constants
 } // inline namespace constants // I added this.

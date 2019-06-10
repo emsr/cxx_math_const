01deafa7d5509fca0ad1daa6ff3d6586138deba4
emsr <3dw4rd@verizon.net>
2019-06-10 14:53:10 -0400

    cxx_math_const: CMake improvements, doxy, testing.

---
+++ docs/html/search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['math_5fconstants_2eh',['math_constants.h',['../math__constants_8h.html',1,'(Global Namespace)'],['../ext_2math__constants_8h.html',1,'(Global Namespace)']]]
+];
